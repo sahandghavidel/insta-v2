c657e2ecf6d90cd7429ff6fd97d549df1e3dd7f7
Sahandgha <sahand.gha@gmail.com>
2022-01-23 08:29:57 +1000

    make feed section resposive

--- components/Feed.js
+++ components/Feed.js
@@ -4,14 +4,14 @@ import Stories from "./Stories";
 
 export default function Feed() {
   return (
-    <main>
-      <section>
+    <main className="grid grid-cols-1 md:grid-cols-2 max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
+      <section className="md:col-span-2">
         {/* Stories */}
         <Stories />
         {/* Posts */}
         <Posts />
       </section>
-      <section>
+      <section className="hidden xl:inline-grid md:col-span-1">
         {/* Mini Profile */}
         {/* Suggestions */}
       </section>
