9fa5587ba6d10ee0c3ccf5123e013d6f9f914385
Sahandgha <sahand.gha@gmail.com>
2022-01-25 08:21:20 +1000

    modify stories component and add the user at the bigenning

--- components/Stories.js
+++ components/Stories.js
@@ -1,9 +1,11 @@
 import minifaker from "minifaker";
+import { useSession } from "next-auth/react";
 import "minifaker/locales/en";
 import { useEffect, useState } from "react";
 import Story from "./Story";
 export default function Stories() {
   const [suggestions, setSuggestions] = useState([]);
+  const { data: session } = useSession();
   useEffect(() => {
     const suggestions = minifaker.array(20, (i) => ({
       username: minifaker.username({ locale: "en" }).toLowerCase(),
@@ -11,10 +13,16 @@ export default function Stories() {
       id: i,
     }));
     setSuggestions(suggestions);
-    console.log(suggestions);
   }, []);
   return (
     <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-none">
+      {session && (
+        <Story
+          img={session.user.image}
+          user={session.user.username}
+          isUser="true"
+        />
+      )}
       {suggestions.map((profile) => (
         <Story key={profile.id} img={profile.img} user={profile.username} />
       ))}
--- components/Story.js
+++ components/Story.js
@@ -1,13 +1,15 @@
-export default function Story({ img, user }) {
+import { PlusIcon } from "@heroicons/react/solid";
+export default function Story({ img, user, isUser }) {
   return (
-    <div>
+    <div className="relative group cursor-pointer">
       <img
         className="object-cover h-14 rounded-full p-[1.5px] border-red-500
-      border-2 cursor-pointer hover:scale-110 transition-transform duration-200
+      border-2 group-hover:scale-110 transition-transform duration-200
       ease-out"
         src={img}
         alt={user}
       ></img>
+      {isUser && <PlusIcon className="absolute top-4 left-4 h-6 text-white " />}
       <p className="text-xs w-14 truncate text-center">{user}</p>
     </div>
   );
