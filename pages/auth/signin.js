726d105c3d95c9bf4d80f1449ecd93ce3f10797d
Sahandgha <sahand.gha@gmail.com>
2022-01-27 15:38:31 +1000

    modify the sign in page

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,24 +1,29 @@
 import { getProviders, signIn } from "next-auth/react";
 import Header from "../../components/Header";
-
 export default function SignIn({ providers }) {
   return (
     <>
       <Header />
-      <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-32 10 px-14 text-center">
+      <div className="flex justify-center space-x-7 mt-20">
         <img
-          src="https://www.androidguys.com/wp-content/uploads/2015/04/insta.png"
+          src="https://superviral.com.au/wp-content/uploads/2021/08/instagix-banner-graphic.png"
           alt=""
-          className="w-[75%]"
+          className="hidden object-cover rotate-6 md:inline-flex md:w-48"
         />
-        <p className="text-sm italic">
-          This is NOT a real app, it is built for educational purposes only
-        </p>
-        <div className="mt-40">
+
+        <div className="">
           {Object.values(providers).map((provider) => (
-            <div key={provider.name}>
+            <div key={provider.name} className="flex flex-col items-center">
+              <img
+                src="https://socodigital.com/wp-content/uploads/2021/03/Instagram.png"
+                alt=""
+                className="w-32 object-cover"
+              />
+              <p className="text-sm italic my-10 text-center">
+                This app is created for learning purposes.
+              </p>
               <button
-                className="bg-blue-500 rounded-lg text-white p-3"
+                className="bg-red-400 rounded-lg text-white p-3 whitespace-nowrap hover:bg-red-500"
                 onClick={() => signIn(provider.id, { callbackUrl: "/" })}
               >
                 Sign in with {provider.name}
